6c9f0d71ea9b5bdd702f8e8d11928303fb882828
mrquackduck <mrquackduckk@gmail.com>
2023-06-02 08:54:27 +0300

    Added RecentArticle model

---
+++ JustEmpire/Models/Classes/RecentArticle.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using JustEmpire.Models.Enums;
+
+namespace JustEmpire.Models.Classes;
+
+/// <summary>
+/// Lighter version of Article model
+/// </summary>
+public class RecentArticle
+{
+    [Key]
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public Language Language { get; set; }
+}
