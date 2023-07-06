41d0f7501ec2b5bee7559fb72304f5bd0f4f3379
mrquackduck <mrquackduckk@gmail.com>
2023-07-06 18:59:28 +0300

    Update

--- JustEmpire/Models/Classes/AcceptModels/Services/EditServiceModel.cs
+++ JustEmpire/Models/Classes/AcceptModels/Services/EditServiceModel.cs
@@ -5,7 +5,6 @@ namespace JustEmpire.Models.Classes.AcceptModels.Services;
 public class EditServiceModel
 {
     public int Id { get; set; }
-    public int AuthorId { get; set; }
     public string Title { get; set; }
     public string TitleImage { get; set; }
     public string Text { get; set; }
