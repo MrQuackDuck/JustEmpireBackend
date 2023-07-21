0bed34eb9a3689722c05dbf1a88e4d8ed81229d5
mrquackduck <mrquackduckk@gmail.com>
2023-07-22 00:44:21 +0300

    Update

---
+++ JustEmpire/Controllers/SearchController.cs
@@ -0,0 +1,31 @@
+﻿using JustEmpire.Attributes;
+using JustEmpire.Models.Enums;
+using JustEmpire.Models.Interfaces;
+using JustEmpire.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JustEmpire.Controllers;
+
+public class SearchController : Controller
+{
+    private ArticleRepository _articleRepository;
+    private ServiceRepository _serviceRepository;
+
+    public SearchController(ArticleRepository articleRepository, ServiceRepository serviceRepository)
+    {
+        _articleRepository = articleRepository;
+        _serviceRepository = serviceRepository;
+    }
+
+    [HttpGet]
+    [LogAction]
+    public List<IPostable> Find(string searchString)
+    {
+        var result = new List<IPostable>();
+        
+        result.AddRange(_articleRepository.GetAll().Where(a => a.Title.ToLower().StartsWith(searchString.ToLower()) && a.Status == Status.POSTED));
+        result.AddRange(_serviceRepository.GetAll().Where(s => s.Title.ToLower().StartsWith(searchString.ToLower()) && s.Status == Status.POSTED));
+
+        return result;
+    }
+}
